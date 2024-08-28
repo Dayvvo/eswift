cd525ebfa2abf26c22e4531825388a8412437c54
napoleonddev <Olaoluwa@pocketfood.io>
2024-08-28 06:40:54 -0400

    reusable button component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0215d85ed312168e"; }
+/******/ 		__webpack_require__.h = function() { return "5fa50734d5b7d45f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/Btn.tsx
@@ -0,0 +1,68 @@
+import { Button, ButtonProps } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+interface CustomButtonProps extends ButtonProps {
+  type?: any;
+  children?: ReactNode;
+  border?: string;
+  color?: string;
+  pt?: string;
+  pb?: string;
+  px?: string;
+  w?: string;
+  size?: string;
+  weight?: string;
+  disabled?: boolean | any;
+  bgColor?: string;
+  hover?: any;
+  onClick?: () => void;
+  [key: string]: any;
+}
+
+const Btn = ({
+  border,
+  color,
+  type,
+  children,
+  pt,
+  pb,
+  px,
+  w,
+  size,
+  weight,
+  disabled,
+  bgColor,
+  hover,
+  onClick,
+  ...rest
+}: CustomButtonProps) => {
+  return (
+    <Button
+      type={type || "button"}
+      onClick={onClick}
+      border={border || "none"}
+      pt={pt || "0.5rem"}
+      pb={pb || "0.5rem"}
+      px={px || "1.5rem"}
+      w={w || "auto"}
+      color={color || "#fff"}
+      fontSize={size || "1rem"}
+      fontWeight={weight || 500}
+      isDisabled={disabled}
+      bgColor={bgColor || "#000"}
+      _hover={
+        hover
+          ? hover
+          : {
+              bgColor: "none",
+              border: "none",
+            }
+      }
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default Btn;
