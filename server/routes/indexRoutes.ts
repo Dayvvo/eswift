9d00c8c40e321ef21ff961c5b042f846b0a7ce58
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-08-29 12:12:55 +0100

    register /blog controller

--- server/routes/indexRoutes.ts
+++ server/routes/indexRoutes.ts
@@ -1,13 +1,17 @@
-import express, { Request, Response } from 'express'
-import axios from 'axios'
-import authController from '../controllers/authController'
-import passport from 'passport'
-import authRoutes from './authRoutes'
+import express, { Request, Response } from "express";
+import propertyRoute from "./propertyRoutes";
+import blogPost from "./blogPostRoute";
+import axios from "axios";
+import authController from "../controllers/authController";
+import passport from "passport";
+import authRoutes from "./authRoutes";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/auth', authRoutes)
+router.use("/auth", authRoutes);
 
-router.use('/property', authRoutes)
+router.use("/property", propertyRoute);
 
-export default router
+router.use("/blog", blogPost);
+
+export default router;
