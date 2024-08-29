0a37386e1b62039c915beb47a62ebc862292e0ff
krispamB <chrystopam@gmail.com>
2024-08-29 02:20:52 +0100

    [fix]: remove user data

--- server/data/seed.data.ts
+++ server/data/seed.data.ts
@@ -1,14 +1,3 @@
 import { AuthProvider, IUser, UserRole } from '../utils/interfaces'
 
-export const adminUsers: Array<IUser> = [
-  {
-    email: 'admin@eswift.com',
-    provider: AuthProvider.EMAIL_SIGNUP,
-    firstName: 'Admin',
-    lastName: 'User',
-    hash: 'adminuser',
-    role: UserRole.ADMIN,
-    isActive: true,
-    isVerified: true,
-  },
-]
+export const adminUsers: Array<IUser> = []
