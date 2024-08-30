90cd51fc015993d0b7bf01f06055262751325a04
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-08-30 11:01:11 +0100

    register single blog post controller

--- server/routes/blogPostRoute.ts
+++ server/routes/blogPostRoute.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post("/create-blogpost", blogPostController.createBlogPost);
 router.put("/update-blogpost/:blogPostId", blogPostController.updatatePost);
+router.get("/get-post/:blogPostId", blogPostController.fetchBlogPostById);
 router.get("/get-posts", blogPostController.fetchBlogPost);
 router.delete("/delete-post/:blogPostId", blogPostController.deleteBlogPost);
 
