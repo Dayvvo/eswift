89a5df797508adaf7af18ee1bed5fc83dabc5e28
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-09-05 05:34:04 +0100

    email routes

--- server/routes/indexRoutes.ts
+++ server/routes/indexRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import propertyRoute from "./propertyRoutes";
 import blogPost from "./blogPostRoute";
+import contact from "./contactUs";
 import axios from "axios";
 import authController from "../controllers/authController";
 import passport from "passport";
@@ -13,5 +14,6 @@ router.use("/auth", authRoutes);
 router.use("/property", propertyRoute);
 
 router.use("/blog", blogPost);
+router.use("/email", contact);
 
 export default router;
