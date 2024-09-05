b30d440f92d34068091748f0552c7c146c47bc20
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-09-05 22:25:08 +0100

    added isAuth middleware

--- server/routes/blogPostRoute.ts
+++ server/routes/blogPostRoute.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import blogPostController from "../controllers/blogPostController";
+import { isAuth } from "../utils/middleware";
 
 const router = Router();
 
-router.post("/create-blogpost", blogPostController.createBlogPost);
-router.put("/update-blogpost/:blogPostId", blogPostController.updatePost);
-router.get("/get-posts", blogPostController.fetchBlogPost);
-router.delete("/delete-post/:blogPostId", blogPostController.deleteBlogPost);
+router.post("/create-blogpost", isAuth, blogPostController.createBlogPost);
+router.put(
+  "/update-blogpost/:blogPostId",
+  isAuth,
+  blogPostController.updatePost
+);
+router.get("/get-posts", isAuth, blogPostController.fetchBlogPost);
+router.delete(
+  "/delete-post/:blogPostId",
+  isAuth,
+  blogPostController.deleteBlogPost
+);
 
 export default router;
