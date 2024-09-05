4435be713bacbfda6cf4f1233c8ca8133df9f84c
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-09-05 05:32:10 +0100

    contact us controller

---
+++ server/controllers/contactUsController.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+// import sendEmail from "../utils/service/email";
+import { validateMailbody } from "../utils/validation";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+export interface MailType {
+  email: string;
+  name: string;
+  message: string;
+  subject: string;
+}
+
+const transporter = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+  tls: { rejectUnauthorized: false },
+});
+
+const sendEmailFunction = async ({
+  email,
+  name,
+  message,
+  subject,
+}: MailType) => {
+  const mailOptions = {
+    from: email,
+    to: process.env.CLIENT_EMAIL,
+    subject: subject,
+    name: name,
+    text: message,
+  };
+
+  try {
+    const send = await transporter.sendMail(mailOptions);
+    return send;
+  } catch (error: any) {
+    throw new Error(`Failed to send email: ${error.message}`);
+  }
+};
+
+class ContactUsController {
+  sendEmail = async (req: Request, res: Response) => {
+    try {
+      const { error, value } = validateMailbody(req.body);
+      console.log("value", value);
+
+      if (error) {
+        return res.status(400).json(error.message);
+      }
+      const mailSent = await sendEmailFunction(value);
+      return res.status(200).json({
+        message: "Email sent successfully",
+        data: mailSent,
+      });
+    } catch (error) {
+      res.status(500).send("Failed to send email");
+    }
+  };
+}
+
+let contactUsController = new ContactUsController();
+
+export default contactUsController;
