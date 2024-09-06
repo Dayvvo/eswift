8476ce1cc708334fa12c367c304223456cf38dd0
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-09-06 09:00:41 +0100

    user controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,10 +1,64 @@
-
+import User from "../models/User";
+import { Request, Response } from "express";
 
 class UserController {
-
-    async verifyUser(req: Request, res: Response) {
-        
+  async verifyUser(req: Request, res: Response) {
+    const { userId } = req.params;
+    const { verification } = req.body;
+    const allowedVerificationStatuses = ["pending", "verified", " "];
+    if (!allowedVerificationStatuses.includes(verification)) {
+      return res.status(400).json({ message: "Invalid verification status" });
     }
+
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: userId },
+        { verification: verification },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json({
+        statusCode: 200,
+        message: "Verification status updated successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).send("Failed to update verification status");
+    }
+  }
+  getAllUsers = async (req: Request, res: Response) => {
+    try {
+      const users = await User.find();
+      return res.status(200).json({
+        message: "Fetched successfully",
+        data: users,
+        statusCode: 200,
+      });
+    } catch (error) {
+      res.status(500).send("Failed to fetch all users");
+    }
+  };
+
+  getUserById = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(200).json({
+        message: "Success",
+        statusCode: 200,
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).send("Internal server error");
+    }
+  };
 }
 
-export default UserController;
+let userController = new UserController();
+
+export default userController;
