334c5e5cfdfa6248cf0607f9b8f6e20cf3a2793f
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-10-04 15:42:46 +0100

    added the duration type

--- server/utils/interfaces/property.interface.ts
+++ server/utils/interfaces/property.interface.ts
@@ -1,25 +1,26 @@
 export enum PropertyOwner {
-  'ESWIFT' = 'ESWIFT',
-  'AFFILIATE' = 'AFFILIATE',
+  "ESWIFT" = "ESWIFT",
+  "AFFILIATE" = "AFFILIATE",
 }
 
 export interface ILocation {
-  state: string
-  lga: string
-  address: string
+  state: string;
+  lga: string;
+  address: string;
 }
 
 export interface IProperty {
-  title: string
-  description: string
-  address: string
-  type: string
-  category: string
-  features: Array<string>
-  price: string
-  owner: PropertyOwner
-  images: Array<string>
-  creatorID: any
-  isActive: boolean,
-  verificationState : 'Pending' | 'Verified' | 'Rejected'
+  title: string;
+  description: string;
+  address: string;
+  type: string;
+  category: string;
+  duration: string;
+  features: Array<string>;
+  price: string;
+  owner: PropertyOwner;
+  images: Array<string>;
+  creatorID: any;
+  isActive: boolean;
+  verificationState: "Pending" | "Verified" | "Rejected";
 }
