2e8c4359f2e25d1fdaebb31b87e90b6b5c348819
dayvvo <dyvvoo@gmail.com>
2024-10-17 15:56:44 +0100

    fix: commit before push

--- server/routes/propertyRoutes.ts
+++ server/routes/propertyRoutes.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import authController from "../controllers/authController";
-import passport from "passport";
 import propertyController from "../controllers/propertyController";
 import { isAdmin, isAuth } from "../utils/middleware";
 
@@ -19,7 +17,8 @@ router
   .get(propertyController.getPropertyById)
   .delete(isAuth, propertyController.deleteProperty);
 
-router.patch('/:id/freeze', isAuth, propertyController.isActiveSwitch)  
+router.patch('/:id/freeze', isAuth, propertyController.isActiveSwitch); 
+
 router.put("/:id/verify", isAuth, isAdmin, propertyController.verifyProperty);
 
 export default router;
