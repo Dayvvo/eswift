bd48b9d4178c6da5611ae78329fe7bf8a9c6316e
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-10-27 14:02:27 +0100

    add footer and padding bottom

--- pages/properties/[id].tsx
+++ pages/properties/[id].tsx
@@ -1,18 +1,27 @@
+import { Footer } from "@/components/footer";
 import NavBar from "@/components/navBar";
 import { PropertyDetails } from "@/components/PropertyDetails";
 import { appBodyPosition } from "@/utils/modules";
-import { Center } from "@chakra-ui/react";
+import { Center, Box } from "@chakra-ui/react";
 
+const PropertiesDetailScreen = () => (
+  <>
+    <NavBar />
+    <Center
+      position={"relative"}
+      top="96px"
+      pt={"2em"}
+      pb={"6em"}
+      className="center"
+      mb={"24px"}
+    >
+      <PropertyDetails clientView />
+    </Center>
 
+    <Box>
+      <Footer />
+    </Box>
+  </>
+);
 
-const PropertiesDetailScreen = () => 
-    (
-        <>
-            <NavBar/>        
-            <Center position={'relative'} top='96px' py={'2em'} className="center">
-               <PropertyDetails clientView/>
-            </Center>
-        </>
-    );
-  
-  export default PropertiesDetailScreen;
+export default PropertiesDetailScreen;
