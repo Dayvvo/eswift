4decd7dc797209fdae19c0345bfb7e29b2c284e7
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-10-29 05:44:53 +0100

    correct typo error on documents type

--- utils/types.ts
+++ utils/types.ts
@@ -1,21 +1,22 @@
-
-
-export type R = Record<string,any>;
+export type R = Record<string, any>;
 
 export type properties = {
-    _id:string;
-    title:string;
-    description:string;
-    price:string;
-    images:string[];
-    duration:string | null;
-    documents?:[]
-    address:string
-}
+  _id: string;
+  title: string;
+  description: string;
+  price: string;
+  images: string[];
+  duration: string | null;
+  documents?: [];
+  address: string;
+};
 
-
-
-export type DocumentTypes = 
-  'FamilyReceipt' | 'SurveyPlan' | 'Layout' | 'Affidavidit' | 
-  'Agreement' | 'CofO' | 'PowerOfAttourney' | 'GovConsent';
-
+export type DocumentTypes =
+  | "FamilyReceipt"
+  | "SurveyPlan"
+  | "Layout"
+  | "Affidavit"
+  | "Agreement"
+  | "CofO"
+  | "PowerOfAttorney"
+  | "GovConsent";
