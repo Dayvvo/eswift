35bce12a1c192d5e56e1c495985d2d433ff6753e
krispamB <chrystopam@gmail.com>
2024-11-01 00:28:06 +0100

    [feat]: update create property endpoint

--- server/utils/interfaces/property.interface.ts
+++ server/utils/interfaces/property.interface.ts
@@ -1,26 +1,35 @@
+import { ObjectId } from "mongoose"
+import { PropertyDocuments, PropertyVerification } from "./types"
+
 export enum PropertyOwner {
-  "ESWIFT" = "ESWIFT",
-  "AFFILIATE" = "AFFILIATE",
+  'ESWIFT' = 'ESWIFT',
+  'AFFILIATE' = 'AFFILIATE',
 }
 
 export interface ILocation {
-  state: string;
-  lga: string;
-  address: string;
+  state: string
+  lga: string
+  address: string
+}
+
+export interface IDocument {
+  type: PropertyDocuments
+  document: string
 }
 
 export interface IProperty {
-  title: string;
-  description: string;
-  address: string;
-  type: string;
-  category: string;
-  duration: string;
-  features: Array<string>;
-  price: string;
-  owner: PropertyOwner;
-  images: Array<string>;
-  creatorID: any;
-  isActive: boolean;
-  verification: "pending" | "verified" | "rejected" | "suspend";
+  title: string
+  description: string
+  address: string
+  type: string
+  category: string
+  duration: string
+  features: Array<string>
+  price: string
+  owner: PropertyOwner
+  images: Array<string>
+  documents: Array<IDocument>
+  creatorID: ObjectId
+  isActive: boolean
+  verification: PropertyVerification
 }
