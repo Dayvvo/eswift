389a16e7849860c4cbc167065a0efc25e118c993
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-11-06 05:40:51 +0100

    added phone number and address to IUser interface

--- server/utils/interfaces/user.interface.ts
+++ server/utils/interfaces/user.interface.ts
@@ -1,43 +1,45 @@
-import { ObjectId } from 'mongoose'
+import { ObjectId } from "mongoose";
 
 export enum UserRole {
-  CLIENT = 'CLIENT',
-  ADMIN = 'ADMIN',
-  AFFILIATE = 'AFFILIATE',
-  AGENT = 'AGENT',
-  GUEST = 'GUEST',
-  STAFF = 'STAFF',
+  CLIENT = "CLIENT",
+  ADMIN = "ADMIN",
+  AFFILIATE = "AFFILIATE",
+  AGENT = "AGENT",
+  GUEST = "GUEST",
+  STAFF = "STAFF",
 }
 
 export enum AuthProvider {
-  GOOGLE = 'google',
-  EMAIL_SIGNUP = 'email_signup',
+  GOOGLE = "google",
+  EMAIL_SIGNUP = "email_signup",
 }
 
 export interface IUser {
-  _id: ObjectId
-  tenantId?: string
-  email: string
-  avatar?: string
-  provider?: AuthProvider
-  lastName: string
-  firstName: string
-  refCode: string
-  refCount: number
-  hash?: string
-  propertyCount: number
-  role: UserRole
-  isActive: boolean
-  passwordUpdated: boolean
-  referrer: ObjectId
-  verification: 'pending' | 'verified' | 'rejected'
-  matchPassword?: FunctionConstructor
-  increasePropertyCount?: FunctionConstructor
-  decreasePropertyCount?: FunctionConstructor
+  _id: ObjectId;
+  tenantId?: string;
+  email: string;
+  avatar?: string;
+  provider?: AuthProvider;
+  lastName: string;
+  firstName: string;
+  refCode: string;
+  refCount: number;
+  hash?: string;
+  propertyCount: number;
+  role: UserRole;
+  isActive: boolean;
+  passwordUpdated: boolean;
+  referrer: ObjectId;
+  verification: "pending" | "verified" | "rejected";
+  phoneNumber: string;
+  address: string;
+  matchPassword?: FunctionConstructor;
+  increasePropertyCount?: FunctionConstructor;
+  decreasePropertyCount?: FunctionConstructor;
 }
 
 export interface IUserInRequest extends IUser {
-  matchPassword: FunctionConstructor
-  increasePropertyCount: FunctionConstructor
-  decreasePropertyCount: FunctionConstructor
+  matchPassword: FunctionConstructor;
+  increasePropertyCount: FunctionConstructor;
+  decreasePropertyCount: FunctionConstructor;
 }
