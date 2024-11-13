aca89ab2335a17570a67d861131d2ba4d8b587d7
bagwells <timmyayomidebagwells@outlook.com>
2024-11-13 13:40:03 +0100

    UI Favourites update

--- screens/Property/property.tsx
+++ screens/Property/property.tsx
@@ -17,7 +17,6 @@ import { useEffect, useState } from "react";
 import { Modal } from "../../components/modal";
 import { BsPlus } from "react-icons/bs";
 import { PlusIcon, SearchIcon } from "../../components/svg";
-
 import useProperty from "@/hooks/useProperty";
 import { useImage, useInputText } from "../../hooks/useInput";
 import { useApiUrl } from "../../hooks/useApi";
