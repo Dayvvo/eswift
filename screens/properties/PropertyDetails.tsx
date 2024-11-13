7984556fb60273217823bb7af300d86d9f02f162
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-11-13 16:54:40 +0100

    add user phone number and comment out property count

--- screens/properties/PropertyDetails.tsx
+++ screens/properties/PropertyDetails.tsx
@@ -18,18 +18,16 @@ import useProperty, { Favourite } from "@/hooks/useProperty";
 import { useEffect, useState } from "react";
 import { useAppContext } from "@/context";
 import { R } from "@/utils/types";
-import {  PropertyCardProps } from "../Property/propertyCard";
+import { PropertyCardProps } from "../Property/propertyCard";
 import { useRouter } from "next/router";
 import { MdLocationOn } from "react-icons/md";
-import {  ZigiZagaIcon } from "../../components/svg";
+import { ZigiZagaIcon } from "../../components/svg";
 import { AxiosError, AxiosResponse } from "axios";
 import useToast from "@/hooks/useToast";
 
 export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
-  
-  
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
-  
+
   const handleImageClick = (index: number) => {
     setSelectedImageIndex(index);
   };
@@ -38,70 +36,73 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
 
   const { globalContext, setGlobalContext } = useAppContext();
 
-  const toast = useToast()
+  const toast = useToast();
 
   const { user } = globalContext;
 
-  const [detailsData, setDetailsData] = useState<PropertyCardProps | null>(null);
+  const [detailsData, setDetailsData] = useState<PropertyCardProps | null>(
+    null
+  );
 
-  const { getPropertyDetails, addToFavorites, deleteFromFavorites } = useProperty();
+  const { getPropertyDetails, addToFavorites, deleteFromFavorites } =
+    useProperty();
 
   const { query, isReady } = useRouter();
 
-  const addToFave = async(id:string)=>{
-    try{
-        const {data} = await addToFavorites(id)  as AxiosResponse ; 
-        if(data){
-            toast.toast({
-                title:'Request successful',
-                status:'success',
-                description:'Property added to favoriites'
-            });
-            setGlobalContext && setGlobalContext(prev=>({
-                ...prev,
-                favourites: [...prev.favourites, data as Favourite] 
-            }))
-            
-        };
+  const addToFave = async (id: string) => {
+    try {
+      const { data } = (await addToFavorites(id)) as AxiosResponse;
+      if (data) {
+        toast.toast({
+          title: "Request successful",
+          status: "success",
+          description: "Property added to favoriites",
+        });
+        setGlobalContext &&
+          setGlobalContext((prev) => ({
+            ...prev,
+            favourites: [...prev.favourites, data as Favourite],
+          }));
+      }
+    } catch (err) {
+      let error = err as AxiosError;
+      if (error?.response) {
+        toast.toast({
+          status: "error",
+          title: "Request successful",
+          description: "Failed to add property added to favoriites",
+        });
+      }
     }
-    catch(err){
-        let error = err as AxiosError;
-        if(error?.response){
-            toast.toast({
-                status:'error',
-                title:'Request successful',
-                description:'Failed to add property added to favoriites'
-            })
-        }   
-    }
-  }
+  };
 
-  const deleteFromFave = async(id:string)=>{
-      try{
-          const {data} = await deleteFromFavorites(id)  as AxiosResponse ; 
-          if(data){
-              toast.toast({
-                  title:'Request successful',
-                  status:'success',
-                  description:'Property removed from favoriites'
-              });
-              setGlobalContext && setGlobalContext(prev=>({
-                  ...prev,
-                  favourites: prev.favourites.filter(prop=> prop?.favoriteId !==id) 
-              }))
-              
-          };
+  const deleteFromFave = async (id: string) => {
+    try {
+      const { data } = (await deleteFromFavorites(id)) as AxiosResponse;
+      if (data) {
+        toast.toast({
+          title: "Request successful",
+          status: "success",
+          description: "Property removed from favoriites",
+        });
+        setGlobalContext &&
+          setGlobalContext((prev) => ({
+            ...prev,
+            favourites: prev.favourites.filter(
+              (prop) => prop?.favoriteId !== id
+            ),
+          }));
       }
-      catch(err){
-          let error = err as AxiosError;
-          if(error?.response){
-              toast.toast({
-                  title:'Request successful',
-                  description:'Failed to remove property from favoriites'
-              })
-          }   
+    } catch (err) {
+      let error = err as AxiosError;
+      if (error?.response) {
+        toast.toast({
+          title: "Request successful",
+          description: "Failed to remove property from favoriites",
+        });
       }
-  }
+    }
+  };
 
   useEffect(() => {
     if (isReady) {
@@ -116,7 +117,6 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
       })();
     }
   }, [isReady]);
-
 
   const isAdmin = user?.role === "Admin";
 
@@ -247,7 +247,6 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
           w="630px"
           h={"514px"}
         >
-
           <Box flex={"1"}>
             <Text
               as={"h2"}
@@ -269,31 +268,7 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
             </Text>
           </Box>
 
-          {
-            detailsData?.lga &&
-            <Box flex={"1"}>
-            <Text
-              as={"h2"}
-              fontWeight={"600"}
-              fontSize={"20px"}
-              className="robotoF"
-              color={"#000"}
-            >
-              State
-            </Text>
-            <Text
-              mt={"6px"}
-              color="#999999"
-              className="robotoF"
-              fontWeight={"500"}
-              fontSize={"16px"}
-            >
-              {detailsData?.state}
-            </Text>
-            </Box>
-          }
-          {
-            detailsData?.state &&
+          {detailsData?.lga && (
             <Box flex={"1"}>
               <Text
                 as={"h2"}
@@ -313,36 +288,57 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
               >
                 {detailsData?.state}
               </Text>
-            </Box>    
-          }
+            </Box>
+          )}
+          {detailsData?.state && (
+            <Box flex={"1"}>
+              <Text
+                as={"h2"}
+                fontWeight={"600"}
+                fontSize={"20px"}
+                className="robotoF"
+                color={"#000"}
+              >
+                State
+              </Text>
+              <Text
+                mt={"6px"}
+                color="#999999"
+                className="robotoF"
+                fontWeight={"500"}
+                fontSize={"16px"}
+              >
+                {detailsData?.state}
+              </Text>
+            </Box>
+          )}
 
           <Box flex={"1"}>
-          <Text
-            as={"h2"}
-            fontWeight={"600"}
-            fontSize={"20px"}
-            className="robotoF"
-            color={"#000"}
-          >
-            Address
-          </Text>
-          <Flex>
-            <MdLocationOn />
             <Text
-              fontSize="14px"
-              maxW={"90%"}
-              overflow={"hidden"}
-              textOverflow={"ellipsis"}
-              className="urbanist"
-              whiteSpace={"nowrap"}
+              as={"h2"}
+              fontWeight={"600"}
+              fontSize={"20px"}
+              className="robotoF"
+              color={"#000"}
             >
-              {detailsData?.address}
+              Address
             </Text>
-          </Flex>
-
+            <Flex>
+              <MdLocationOn />
+              <Text
+                fontSize="14px"
+                maxW={"90%"}
+                overflow={"hidden"}
+                textOverflow={"ellipsis"}
+                className="urbanist"
+                whiteSpace={"nowrap"}
+              >
+                {detailsData?.address}
+              </Text>
+            </Flex>
           </Box>
 
-          <Box flex={'1'}>
+          <Box flex={"1"}>
             <Text
               as={"h2"}
               fontWeight={"600"}
@@ -362,7 +358,6 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
               {detailsData?.price?.amount}
             </Text>
           </Box>
-          
 
           {/* <Flex
             borderTop={"1px solid #262626"}
@@ -445,43 +440,39 @@ export const PropertyDetails = ({ clientView }: { clientView?: boolean }) => {
             >
               Contact
             </Btn>
-            
-            
-              {
-                detailsData?.isFavorite? 
-                
-                <Btn
-                  border="1px solid #FB3748"
-                  borderRadius={"10px"}
-                  padding={"10px"}
-                  color="#FB3748"
-                  className="robotoF"
-                  bgColor="transparent"
-                  w="100%"
-                  onClick={()=>addToFave(detailsData?._id as string)}
-                  isLoading={loadingBtn}
-                >
-                  Add to favorites 
-                </Btn>
-                :
-                <Btn
-                 border="1px solid #FB3748"
-                 borderRadius={"10px"}
-                 padding={"10px"}
-                 color="#FB3748"
-                 className="robotoF"
-                 bgColor="transparent"
-                 w="100%"
-                 isLoading={loadingBtn}
-                 onClick={()=>deleteFromFave(detailsData?.favoriteId as string)}
-                >
-                  Remove from favorites 
-                </Btn>
-              }
-          
-      
+
+            {detailsData?.isFavorite ? (
+              <Btn
+                border="1px solid #FB3748"
+                borderRadius={"10px"}
+                padding={"10px"}
+                color="#FB3748"
+                className="robotoF"
+                bgColor="transparent"
+                w="100%"
+                onClick={() => addToFave(detailsData?._id as string)}
+                isLoading={loadingBtn}
+              >
+                Add to favorites
+              </Btn>
+            ) : (
+              <Btn
+                border="1px solid #FB3748"
+                borderRadius={"10px"}
+                padding={"10px"}
+                color="#FB3748"
+                className="robotoF"
+                bgColor="transparent"
+                w="100%"
+                isLoading={loadingBtn}
+                onClick={() =>
+                  deleteFromFave(detailsData?.favoriteId as string)
+                }
+              >
+                Remove from favorites
+              </Btn>
+            )}
           </Flex>
- 
         </Box>
         <Box
           borderRadius={"10px"}
